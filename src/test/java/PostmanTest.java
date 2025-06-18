927c9f35a1849d7d76dbd38470b6b34d733a72c3
Natalia <natala.serdakova562@gmail.com>
2025-06-18 22:23:25 +0300

    Initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanTest {
+    @Test
+    void shouldData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+    }
+}
+
